d58825a20da3b4f26a9e224f7507d3ebb65b8c72
7449 <yuezhaoyang7449@163.com>
2019-03-15 17:14:20 +0800

    Update React-Native Version

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,12 +1,14 @@
+/**
+ * @format
+ */
+
 import 'react-native';
 import React from 'react';
-import App from '../app/App';
+import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <App />
-  );
+  renderer.create(<App />);
 });
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+/**
+ * @format
+ */
+
 import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
 import NavigatorManager from './app/NavigatorManager';
 
-AppRegistry.registerComponent('rnwidget', () => NavigatorManager);
+AppRegistry.registerComponent(appName, () => NavigatorManager);
